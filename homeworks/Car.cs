93aa7d8f87d9b9617da0e0bd488c0b797ffba753
Tomáš Patloka <tpatloka@gmail.com>
2024-12-11 18:34:30 +0100

    třída Cars

---
+++ homeworks/Car.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Lekce7
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+        public Car(string brand, string model, int year, int mileage)
+        {
+            Brand = brand;
+            Model = model;
+            Year = year;
+            Mileage = mileage;
+        }
+
+        public void Drive(int kilometers)
+        {
+            Mileage += kilometers;
+        }
+
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}, Model: {Model}, Year: {Year}, Mileage: {Mileage} km");
+        }
+    }
+}
