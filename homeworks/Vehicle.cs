ed69020e950afefdbc09b96e707bfd76f249f6e5
Tomáš Patloka <tpatloka@gmail.com>
2024-12-11 20:26:01 +0100

    třída Vehicle dle zadání

---
+++ homeworks/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿namespace homeworks
+{
+    abstract class Vehicle
+    {
+        public int Id;
+        public string Brand;
+        public int Year;
+
+        public Vehicle(int id, string brand, int year)
+        {
+            Id = id;
+            Brand = brand;
+            Year = year;
+        }
+
+        public abstract void StartEngine();
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine("ID: " + Id + ", Značka: " + Brand + ", Rok: " + Year);
+
+        } 
+        
+
+    }
+    
+}
