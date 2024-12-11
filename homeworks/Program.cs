4ad5387742522f4c2d16ff71021b292b94b966e9
Tomáš Patloka <tpatloka@gmail.com>
2024-12-11 18:33:09 +0100

    vytvoření hw6

--- homeworks/Program.cs
+++ homeworks/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace homeworks
+﻿namespace Lekce7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Vytvoření a testování objektů třídy Car
+            Car car1 = new Car("Toyota", "Corolla", 2020, 15000);
+            Car car2 = new Car("Škoda", "Octavia", 2022, 5000);
+
+            car1.Drive(100);
+            car2.Drive(250);
+
+            car1.DisplayCarInfo();
+            car2.DisplayCarInfo();
+
+            Console.WriteLine();
+
+            // Vytvoření a testování objektů třídy Book
+            Book book1 = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180);
+            Book book2 = new Book("1984", "George Orwell", 328);
+
+            book1.Read(50);
+            book2.Read(100);
+
+            book1.DisplayProgress();
+            book2.DisplayProgress();
+
+            // Zkouška přečtení více stránek než kniha obsahuje
+            book1.Read(200);
+            book1.DisplayProgress();
         }
     }
 }
