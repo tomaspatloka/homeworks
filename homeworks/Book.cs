619d84867e0748cc80027739017a076702d414af
Tomáš Patloka <tpatloka@gmail.com>
2024-12-11 18:37:17 +0100

    třída Book

---
+++ homeworks/Book.cs
@@ -0,0 +1,29 @@
+﻿namespace Lekce7
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public int CurrentPage { get; set; }
+
+        public Book(string title, string author, int pages)
+        {
+            Title = title;
+            Author = author;
+            Pages = pages;
+            CurrentPage = 0;
+        }
+
+        public void Read(int pages)
+        {
+            int newPage = CurrentPage + pages;
+            CurrentPage = Math.Min(newPage, Pages);
+        }
+
+        public void DisplayProgress()
+        {
+            Console.WriteLine($"Reading '{Title}' by {Author}: {CurrentPage}/{Pages} pages read");
+        }
+    }
+}
