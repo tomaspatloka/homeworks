b5fde9f208332d2a9720a6320855cca7772981a0
Tomáš Patloka <tpatloka@gmail.com>
2024-12-15 18:25:00 +0100

    trida program..

--- homeworks/Program.cs
+++ homeworks/Program.cs
@@ -6,14 +6,16 @@
         {
             IDriveable[] vehicles1 = new IDriveable[150];
 
-            //tady nejak naplnit pole auty , moto a trucky
+            //tady nejak... naplnit pole auty , moto a trucky asi ...
             for (int i = 0; i < 50; i++)
             {
                 vehicles1[i] = new Car(i + 1, "Škoda", 2024, 5);
-
+                vehicles1[i + 50] = new Motorcycle(i + 51, "Honda", 2021, false);
+                vehicles1[i + 100] = new Truck(i + 101, "Volvo", 2019, 10);
 
             }
+        }   
 
-        }
+            /// co dál .??
     }
 }
