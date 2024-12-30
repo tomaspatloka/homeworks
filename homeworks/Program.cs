4e4249506cc478f9b9a5dd31c4f7105a10c77f3d
Tomáš Patloka <tpatloka@gmail.com>
2024-12-30 19:33:43 +0100

    kod pro vzorova data

--- homeworks/Program.cs
+++ homeworks/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace homeworks
+﻿using System.Globalization;
+
+namespace Lekce10
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var random = new Random();
+            var names = new List<string>
+            { "Alice",
+              "Bob",
+              "Charlie",
+              "Diana",
+              "Edward",
+              "Fiona",
+              "George",
+              "Hannah",
+              "Ian",
+              "Julia"
+            };
+
+            var students = new List<Student>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                var student = new Student
+                {
+                    Name = names[random.Next(names.Count)] + i,
+                    Age = random.Next(18, 25),
+                    Grades = new List<int>
+                    {
+                        random.Next(20, 100),
+                        random.Next(50, 100),
+                        random.Next(70, 100)
+                    }
+                };
+
+                students.Add(student);
+            }
         }
     }
 }
