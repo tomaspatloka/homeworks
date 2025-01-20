58afaa3347dad85de7a42df2e5337d26cab82846
Tomáš Patloka <tpatloka@gmail.com>
2025-01-20 21:13:41 +0100

    test tridy

---
+++ WeatherApp.Tests/WeatherDisplayTests.cs
@@ -0,0 +1,54 @@
+﻿namespace WeatherApp.Tests
+{
+    public class WeatherDisplayTests
+    {
+        
+        private WeatherDisplay _weatherDisplay;
+        private StringWriter _stringWriter;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+            _weatherDisplay = new WeatherDisplay();
+            _stringWriter = new StringWriter();
+            Console.SetOut(_stringWriter);
+        }
+
+        // Test s testovacími daty 
+        [TestCase("Praha", 20.5, "Slunečno")]  
+        [TestCase("Brno", 18.5, "Oblačno")]    
+        [TestCase("Ostrava", 15.0, "Déšť")]    
+        public void ZobrazPocasi_ValidData_DisplaysCorrectly(string mesto, double teplota, string pocasi)
+        {
+            // Arrange - příprava testovacích dat
+            var weatherData = new WeatherData
+            {
+                Mesto = mesto,
+                Teplota = teplota,
+                StavPocasi = pocasi,
+                RychlostVetru = 15.5,
+                Vlhkost = 45,
+                Aktualizace = "2025-01-20 14:30"
+            };
+
+            _weatherDisplay.ZobrazPocasi(weatherData);
+            string output = _stringWriter.ToString();
+
+            // ověření, že výstup obsahuje očekávané hodnoty
+            Assert.That(output.Contains(mesto));         
+            Assert.That(output.Contains(teplota.ToString())); 
+            Assert.That(output.Contains(pocasi));      
+        }
+
+        
+
+        // Metoda pro úklid po každém testu
+        [TearDown]
+        public void Cleanup()
+        {
+            
+            _stringWriter?.Dispose();
+        }
+    }
+}
---
+++ WeatherApp.Tests/WeatherServiceTests.cs
@@ -0,0 +1,54 @@
+﻿using Moq;
+
+namespace WeatherApp.Tests
+{
+    public class WeatherServiceTests
+    {
+        private Mock<IWeatherService> _weatherServiceMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _weatherServiceMock = new Mock<IWeatherService>();
+        }
+
+
+        // Test ověřuje správné fungování, že vrácená data odpovídají ..
+        [Test]
+        public async Task GetWeatherDataAsync_ValidData_ReturnsWeatherData()
+        {
+            var expectedData = new WeatherData
+            {
+                Mesto = "Praha",
+                Teplota = 20.5,
+                StavPocasi = "Slunečno",
+                RychlostVetru = 15.5,
+                Vlhkost = 45,
+                Aktualizace = "2024-01-20 14:30"
+            };
+
+            _weatherServiceMock
+                .Setup(x => x.GetWeatherDataAsync("Praha"))
+                .ReturnsAsync(expectedData);
+            
+            var result = await _weatherServiceMock.Object.GetWeatherDataAsync("Praha");
+            
+            Assert.That(result.Mesto, Is.EqualTo("Praha"), "Město by měloi být Praha");
+            Assert.That(result.Teplota, Is.EqualTo(20.5), "Teplota by měla být 20.5");
+            Assert.That(result.StavPocasi, Is.EqualTo("Slunečno"), "Stav počasí by měl být Slunečno");
+        }
+        //Tento test ověřuje situaci, kdy se služba pokusí získat data o počasí pro neexistující město.
+        [Test]
+        public void GetWeatherDataAsync_InvalidCity_ThrowsException()
+        {
+            // Arrange
+            _weatherServiceMock
+                .Setup(x => x.GetWeatherDataAsync("NeexistujiciMesto"))
+                .ThrowsAsync(new HttpRequestException());
+
+            // Act & Assert
+            Assert.That(async () => await _weatherServiceMock.Object.GetWeatherDataAsync("NeexistujiciMesto"),
+            Throws.TypeOf<HttpRequestException>());
+        }
+    }
+}
